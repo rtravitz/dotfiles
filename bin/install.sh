668105b7616db9dea3abcd052401d4eccd0cb323
Ryan Travitz <rtravitz@gmail.com>
2020-04-28 10:07:23 -0400

    Simplify line filtering in install script

--- bin/install.sh
+++ bin/install.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # grab lines from packages.txt that don't start with a '#'
-# and brew install each of them
-cat packages.txt | awk '/^[^#\s].+/ { print $1 }' | xargs -I % brew install %
+# and are not blank and brew install each of them
+cat packages.txt | grep -Ev '(^#|^$)' | xargs -I % brew install %
 echo '🛠️ install script complete'
 
