bd393c279dde56eb8cc955232d2847e620df4399
Ryan Travitz <rtravitz@gmail.com>
2022-11-15 12:43:05 -0800

    Update lua lsp server config to show nvim path

--- config/nvim/lua/lsp.lua
+++ config/nvim/lua/lsp.lua
@@ -37,10 +37,6 @@ end
 
 require'lspconfig'.eslint.setup{}
 
-local runtime_path = vim.split(package.path, ';')
-table.insert(runtime_path, "lua/?.lua")
-table.insert(runtime_path, "lua/?/init.lua")
-
 require'lspconfig'.sumneko_lua.setup {
   capabilities = capabilities,
   on_attach = on_attach,
@@ -49,8 +45,6 @@ require'lspconfig'.sumneko_lua.setup {
       runtime = {
         -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
         version = 'LuaJIT',
-        -- Setup your lua path
-        path = runtime_path,
       },
       diagnostics = {
         -- Get the language server to recognize the `vim` global
--- config/nvim/lua/plugins/cmp.lua
+++ config/nvim/lua/plugins/cmp.lua
@@ -23,7 +23,7 @@ cmp.setup({
     { name = 'nvim_lsp' },
     { name = 'vsnip' },
   }, {
-    { name = 'buffer' },
+    { name = 'buffer', keyword_length = 5 },
   })
 })
 
