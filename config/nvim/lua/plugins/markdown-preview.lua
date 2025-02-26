96c268181f26dbb8ddd271e9bffda1fda04cb19e
Ryan Travitz <rtravitz@gmail.com>
2025-02-26 09:26:00 -0800

    Update how markdown-preview plugin is installed

--- config/nvim/lua/plugins/markdown-preview.lua
+++ config/nvim/lua/plugins/markdown-preview.lua
@@ -1,4 +1,9 @@
 return {
   'iamcco/markdown-preview.nvim',
-  build = function() vim.fn["mkdp#util#install"]() end,
+  cmd = { 'MarkdownPreviewToggle', 'MarkdownPreview', 'MarkdownPreviewStop' },
+  ft = { 'markdown' },
+  build = function()
+    vim.cmd('Lazy load markdown-preview.nvim')
+    vim.fn['mkdp#util#install']()
+  end,
 }
