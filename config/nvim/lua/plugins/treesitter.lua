f9f60b5ebb596c9dfcdb4184b732a483b8c21369
Ryan Travitz <rtravitz@gmail.com>
2026-02-03 22:38:22 -0800

    Update plugin versions and how nvim-treesitter is configured

--- config/nvim/lua/plugins/treesitter.lua
+++ config/nvim/lua/plugins/treesitter.lua
@@ -1,28 +1,12 @@
 return {
   {
     "nvim-treesitter/nvim-treesitter",
-    event = "BufReadPost",
-    build = function()
-      pcall(require('nvim-treesitter.install').update { with_sync = true })
-    end,
-    config = function(_, opts)
-      require('nvim-treesitter.configs').setup(opts)
-    end,
+    lazy = false,
+    build = ':TSUpdate',
     opts = {
-      ensure_installed = 'all',
-      -- https://github.com/claytonrcarter/tree-sitter-phpdoc/issues/15
       ignore_install = { 'phpdoc', 'ipkg' },
       highlight = { enable = true },
       indent = { enable = false },
-      incremental_selection = {
-        enable = true,
-        keymaps = {
-          init_selection = '<c-space>',
-          node_incremental = '<c-space>',
-          scope_incremental = '<c-s>',
-          node_decremental = '<c-backspace>',
-        },
-      },
     }
   }
 }
