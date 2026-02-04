a1fcc902952f422f67ee92f0fa03aba0dae7a9a9
Ryan Travitz <rtravitz@gmail.com>
2026-02-03 22:30:20 -0800

    Update arch-install script to break up sections

--- bin/install-arch.sh
+++ bin/install-arch.sh
@@ -1,39 +1,73 @@
 #!/usr/bin/env bash
 
-sudo pacman -S \
-  bash-completion \
-  bat \
-  clang \
-  fd \
-  fzf \
-  git \
-  git-delta \
-  github-cli \
-  jq \
-  less \
-  ripgrep \
-  tmux \
-  unzip \
-  firefox \
-  ghostty \
-  spotify-launcher \
-  hyprpaper \
-  hyprlock \
-  bluez \
-  bluez-utils \
-  bluetui \
-  blueman \
-  pipewire \
-  pipewire-audio \
-  pipewire-pulse \
-  pipewire-alsa \
-  wireplumber \
-  playerctl \
-  pavucontrol \
-  networkmanager \
-  waybar \
-  which \
-  less \
-  ttf-nerd-fonts-symbols \
+install() {
+  echo $1 | xargs sudo pacman -S --noconfirm
+}
+
+# Utilities
+install "
+  bash-completion 
+  bat 
+  clang 
+  fd 
+  fzf 
+  git 
+  git-delta 
+  github-cli 
+  jq 
+  less 
+  less 
+  ripgrep 
+  tmux 
+  unzip 
+  which
+"
+
+# Apps
+install "
+  firefox
+  ghostty
+  spotify-launcher
+"
+
+# Hyprland
+install " 
+  hyprpaper 
+  hyprlock
+"
+
+# Bluetooth
+install " 
+  bluez 
+  bluez-utils 
+  bluetui 
+  blueman 
+"
+
+# Audio
+install " 
+  pipewire 
+  pipewire-audio 
+  pipewire-pulse 
+  pipewire-alsa 
+  wireplumber 
+  playerctl 
+  pavucontrol
+"
+
+# Secure Boot
+install " 
   sbctl
+"
+
+# Network
+install " 
+  networkmanager
+"
+
+# Waybar
+install " 
+  waybar 
+  ttf-nerd-fonts-symbols 
+"
 
